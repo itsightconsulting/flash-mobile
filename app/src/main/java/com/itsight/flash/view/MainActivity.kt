6b62a864c94047266fe184db223fd37e394cf3b5
peterzinho16 <peterpaul.0194@gmail.com>
2020-02-04 11:48:53 -0500

    implementación de crashlytics

--- app/src/main/java/com/itsight/flash/view/MainActivity.kt
+++ app/src/main/java/com/itsight/flash/view/MainActivity.kt
@@ -11,15 +11,27 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.itsight.flash.R
 import kotlinx.android.synthetic.main.navigation_activity.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, UUID.randomUUID().toString())
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image")
+        firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+
         setContentView(R.layout.navigation_activity)
 
         //Load nav host fragment
