c001c8799d4d6011988f9a7d51ce45c62cb69407
peterzinho16 <peterpaul.0194@gmail.com>
2020-02-10 16:05:59 -0500

    error fg

---
+++ app/src/main/java/com/itsight/flash/view/ErrorFragment.kt
@@ -0,0 +1,31 @@
+package com.itsight.flash.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.itsight.flash.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ErrorFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.error_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
