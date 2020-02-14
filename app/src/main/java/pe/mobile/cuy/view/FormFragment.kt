9c4583e44a6390ef87b3f95709a203d1ee34a53e
Diego <itsig@LAPTOP-6L460DO3>
2020-02-14 11:38:39 -0500

    form fragment

--- app/src/main/java/pe/mobile/cuy/view/FormFragment.kt
+++ app/src/main/java/pe/mobile/cuy/view/FormFragment.kt
@@ -37,10 +37,10 @@ class FormFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Datos preview
-        etName.setText("Miranda")
-        etLastName.setText("Pedrosa")
-        etEmail.setText("mail@mail.com")
-        etDateOfBirth.setText("10/06/1980")
+        //etName.setText("Miranda")
+        //etLastName.setText("Pedrosa")
+        //etEmail.setText("mail@mail.com")
+        //etDateOfBirth.setText("10/06/1980")
         //rbDoNotWantToPort.isChecked = true
 
         oActivation = UserPrefs.getActivation(FlashApplication.appContext)
