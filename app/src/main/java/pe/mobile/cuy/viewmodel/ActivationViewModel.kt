4b0a0df87e40e83d023411051b5138a5d5541b79
Diego <itsig@LAPTOP-6L460DO3>
2020-02-14 18:32:25 -0500

    activation view model

--- app/src/main/java/pe/mobile/cuy/viewmodel/ActivationViewModel.kt
+++ app/src/main/java/pe/mobile/cuy/viewmodel/ActivationViewModel.kt
@@ -1,6 +1,21 @@
 package pe.mobile.cuy.viewmodel
 
 import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.disposables.CompositeDisposable
+import pe.mobile.cuy.service.module.ActivationService
+import javax.inject.Inject
 
 class ActivationViewModel(application: Application) : BaseViewModel(application) {
+
+    private val composite = CompositeDisposable()
+
+    @Inject
+    lateinit var activationService: ActivationService
+
+    val loading = MutableLiveData<Boolean>()
+    val loadError = MutableLiveData<Boolean>()
+    var errorMessage: String = ""
+
+    
 }
