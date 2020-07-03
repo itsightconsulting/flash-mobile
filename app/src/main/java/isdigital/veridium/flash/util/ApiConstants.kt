28bf332af1c8ae39d3ff9e119bc820bf8c4ef695
DiegoHerrera <diego11498@gmail.com>
2020-07-02 22:24:38 -0500

    change url pin activaction mode dev

--- app/src/main/java/isdigital/veridium/flash/util/ApiConstants.kt
+++ app/src/main/java/isdigital/veridium/flash/util/ApiConstants.kt
@@ -2,5 +2,5 @@ package isdigital.veridium.flash.util
 
 const val MAX_BAR_SCANNER_TEMPS = 3
 const val MAX_BIOMETRIC_SCANNER_TEMPS = 3
-const val PORTABILITY_WEB_VIEW = "https://activacionws.flashmobile.pe/pin-portabilidad/"
-//const val PORTABILITY_WEB_VIEW = "http://simactivation.aws.acn-wdt.com/pin-portabilidad/"
+//const val PORTABILITY_WEB_VIEW = "https://activacionws.flashmobile.pe/pin-portabilidad/"
+const val PORTABILITY_WEB_VIEW = "http://simactivation.aws.acn-wdt.com/pin-portabilidad/"
