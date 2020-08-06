c0a43bdda21763875dacc46479f6f60571ba5de4
DiegoHerrera <diego11498@gmail.com>
2020-08-06 18:14:57 -0500

    margin bottom btn error scan, prepare prod enviroment

--- app/src/main/java/isdigital/veridium/flash/util/InfoApi.kt
+++ app/src/main/java/isdigital/veridium/flash/util/InfoApi.kt
@@ -21,10 +21,10 @@ val API_BASE_URL = getAPIBaseURLByProfile(API_PROFILE)
 const val GENERIC_ERROR_MESSAGE = "Estamos teniendo incovenientes. Inténtelo nuevamente más tarde"
 const val TOKEN_ERROR_MESSAGE = "Hemos teniendo un incoveniente. Vuelve a intentarlo ahora"
 
-const val API_BASE = "https://services.insolutions.pe"
+const val API_BASE = "https://services.insolutions.pe" // DEV
 //const val API_BASE = "http://200.121.128.89"
 
-const val API_VERSION_V1 = "/FlashMobile/api/v1"
+const val API_VERSION_V1 = "/FlashMobile/api/v1" // DEV
 //const val API_VERSION_V1 = "/api/v1"
 
 
